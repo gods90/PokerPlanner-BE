bd16baf856bbc23a9f115bd6e45a29db07ad256f
Himank Goel <himank.goel@joshtechnologygroup.com>
2021-10-09 18:59:51 +0530

    Removed commented code

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from django.urls import path, include
+from django.urls import path
 
 from user.views import UserViewSet
 
-# router = DefaultRouter()
-# router.register('', UserViewSet)
-
 urlpatterns = [
-    # path("", include(router.urls)),
     path("", UserViewSet.as_view())
 ]
