3df108a7e463d6cdb8c5f052c398b93cbc6c8a0e
Himank Goel <himank.goel@joshtechnologygroup.com>
2021-10-09 18:57:40 +0530

    Added user api

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,10 @@ from django.urls import path, include
 
 from user.views import UserViewSet
 
-router = DefaultRouter()
-router.register('', UserViewSet)
+# router = DefaultRouter()
+# router.register('', UserViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    # path("", include(router.urls)),
+    path("", UserViewSet.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import generics
 
 from user.models import User
 from user.serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(generics.RetrieveUpdateDestroyAPIView, generics.CreateAPIView):
     """
     User View for Performing CRUD operations.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_object(self):
+        return self.request.user
