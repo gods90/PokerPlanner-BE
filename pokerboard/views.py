2077a91988b84be925c3fade0639eefc91fb4bc6
Yogendra <yogendra.manral@joshtechnologygroup.com>
2021-10-18 11:07:44 +0530

    Pokerboard list of current manager

--- pokerboard/views.py
+++ pokerboard/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from pokerboard.models import Pokerboard
 from pokerboard.serializers import PokerBoardCreationSerializer, PokerBoardSerializer
-
 
 class PokerBoardViewSet(viewsets.ModelViewSet):
     """
@@ -27,4 +27,16 @@ class PokerBoardViewSet(viewsets.ModelViewSet):
         request.data['manager_id'] = request.user.id
         return super().create(request, *args, **kwargs)
 
+    def list(self, request, *args, **kwargs):
+        """
+        Shows the list of pokerboard of current manager
+        """
+        queryset = Pokerboard.objects.filter(manager=request.user)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
