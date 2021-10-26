f3831a860355f72a481a71bc3a857f4df07e261d
Himank Goel <himank.goel@joshtechnologygroup.com>
2021-10-26 12:20:03 +0530

    Fixed session view

--- session/views.py
+++ session/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
 
 from session.models import Session
 from session.serializers import SessionSerializer, SessionCreateSerializer
@@ -17,20 +13,8 @@ class SessionViewSet(viewsets.ModelViewSet):
     serializer_class = SessionSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_serializer(self, *args, **kwargs):
-        # import pdb
-        # pdb.set_trace()
+    def get_serializer_class(self):
         if(self.request.method == 'POST'):
             return SessionCreateSerializer
-        return super().get_serializer(*args, **kwargs)
+        return super().get_serializer_class()
 
-    def create(self, request, *args, **kwargs):
-        """
-            Create new session
-            Required : Token in header, Title, tickets/sprint/jql(atleast one)
-        """
-        serializer = self.get_serializer(data = {**request.data})
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
