d151c5bb06b1447b46f8b94410427569bc086ad6
Himank Goel <himank.goel@joshtechnologygroup.com>
2021-10-27 14:08:24 +0530

    Added newlines

--- invite/email_send.py
+++ invite/email_send.py
@@ -5,10 +5,12 @@ from django.conf import settings
 
 def send_invite_mail(manager_email, recipient):
     context = {
-        'manager_email' : manager_email 
+        'manager_email': manager_email
     }
     email_subject = 'Invitation to pokerboard',
-    email_text_body = render_to_string('invite_pokerboard_email_msg.txt', context)
+    email_text_body = render_to_string(
+        'invite_pokerboard_email_msg.txt', context
+    )
     email = EmailMessage(email_subject, email_text_body, settings.DEFAULT_FROM_EMAIL,
-                                   [recipient, ], )
+                         [recipient, ], )
     return email.send(fail_silently=False)
