4b5f26181e4b771869c0dea0c5eb44486e3bead8
Himank Goel <himank.goel@joshtechnologygroup.com>
2021-10-28 20:21:28 +0530

    jwt token for invite added

--- invite/email_send.py
+++ invite/email_send.py
@@ -5,12 +5,16 @@ from django.conf import settings
 
 import jwt
 
+
 def send_invite_mail(manager_email, recipient, invite_id):
-    token = jwt.encode({'invite_id' : invite_id}, settings.SECRET_KEY)
+    token = jwt.encode(
+        {'invite_id': invite_id},
+        settings.SECRET_KEY, settings.JWT_HASHING_ALGORITHM
+    )
     context = {
         'manager_email': manager_email,
-        'token' : token,
-        'domain_name' : "http://127.0.0.1:5500/#!/signup?token="
+        'token': token,
+        'domain_name': 'http://127.0.0.1:5500/#!/signup'
     }
     email_subject = 'Invitation to pokerboard',
     html_body = render_to_string(
@@ -18,5 +22,4 @@ def send_invite_mail(manager_email, recipient, invite_id):
     )
     plain_text_body = strip_tags(html_body)
     send_mail(email_subject, plain_text_body, settings.DEFAULT_FROM_EMAIL,
-                         [recipient, ], html_message=html_body)
-   
+              [recipient, ], html_message=html_body)
--- invite/serializer.py
+++ invite/serializer.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 
 from invite.models import Invite
 from invite.tasks import send_invite_email_task
+
 from pokerboard import constants
 from pokerboard.models import Pokerboard
 
 from group.models import Group
 
 from user.models import User
+
 
 class InviteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,7 +27,7 @@ class InviteSerializer(serializers.ModelSerializer):
         rep['status'] = constants.INVITE_STATUS[rep['status']][1]
         rep['pokerboard_title'] = instance.pokerboard.title
         return rep
-    
+
     def validate(self, attrs):
         invite = attrs['invite_id']
         if invite.status == constants.DECLINED:
@@ -33,8 +35,7 @@ class InviteSerializer(serializers.ModelSerializer):
         if invite.status == constants.ACCEPTED:
             raise serializers.ValidationError('Invite already accepted!')
         return super().validate(attrs)
-    
-    
+
 
 class InviteCreateSerializer(serializers.Serializer):
     pokerboard = serializers.PrimaryKeyRelatedField(
@@ -44,7 +45,7 @@ class InviteCreateSerializer(serializers.Serializer):
     email = serializers.EmailField(required=False)
     user_role = serializers.ChoiceField(
         choices=constants.ROLE_CHOICES, required=False)
-    
+
     def create(self, attrs):
         pokerboard = attrs['pokerboard']
         users = []
@@ -58,11 +59,15 @@ class InviteCreateSerializer(serializers.Serializer):
                 users.append(user)
             except User.DoesNotExist as e:
                 pokerboard_manager_email = pokerboard.manager.email
-                invite = Invite.objects.filter(email=attrs['email'], pokerboard_id=pokerboard.id)
+                invite = Invite.objects.filter(
+                    email=attrs['email'], pokerboard_id=pokerboard.id)
                 if not invite.exists():
+                    # TODO Resend token if token expired.
                     invite = Invite.objects.create(**attrs)
-                    send_invite_email_task.delay(pokerboard_manager_email, [attrs['email']], invite.id)
-                raise serializers.ValidationError("Email to signup in pokerplanner has been sent.Please check your email.")
+                    send_invite_email_task.delay(pokerboard_manager_email, [
+                                                 attrs['email']], invite.id)
+                raise serializers.ValidationError(
+                    "Email to signup in pokerplanner has been sent.Please check your email.")
         else:
             raise serializers.ValidationError('Provide group_id/email!')
 
@@ -83,25 +88,18 @@ class InviteCreateSerializer(serializers.Serializer):
         for user in users:
             # invite = Invite.objects.update_or_create( pokerboard_id = pokerboard.id, user_id = user.id, defaults={'status' : constants.PENDING,'user_role' : role})
             try:
-                invite = Invite.objects.get(pokerboard_id = pokerboard.id, email = user.email)
+                invite = Invite.objects.get(
+                    pokerboard_id=pokerboard.id, email=user.email)
                 invite.status = constants.PENDING
                 if 'user_role' in attrs.keys():
                     invite.user_role = attrs['user_role']
                 invite.save()
             except Invite.DoesNotExist:
                 new_data = {
-                    'pokerboard_id' : pokerboard.id,
-                    'email' : user.email
+                    'pokerboard_id': pokerboard.id,
+                    'email': user.email
                 }
                 if 'user_role' in attrs.keys():
                     new_data['user_role'] = attrs['user_role']
                 invite = Invite.objects.create(**new_data)
         return invite
-
-        
-
-
-
-
-    
-    
