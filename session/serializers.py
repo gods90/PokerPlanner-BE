f9279140601ca689508ed1f785273a2bd49a523e
Himank Goel <himank.goel@joshtechnologygroup.com>
2021-11-09 15:06:17 +0530

    Review fixes

--- session/serializers.py
+++ session/serializers.py
@@ -1,8 +1,17 @@
+from django.db.models.expressions import Case, Value, When, F
+
 from rest_framework import serializers
 
-from pokerboard.models import Pokerboard
+from pokerboard.models import Pokerboard, Ticket
 
 from session.models import Session
+
+class TicketSerializerSessionCreate(serializers.Serializer):
+    """
+    Serializer for validating tickets when creating new session
+    """
+    ticket_id = serializers.CharField()
+    order = serializers.IntegerField()
 
 
 class SessionSerializer(serializers.ModelSerializer):
@@ -13,9 +22,23 @@ class SessionSerializer(serializers.ModelSerializer):
     pokerboard = serializers.PrimaryKeyRelatedField(
         queryset=Pokerboard.objects.all()
     )
+    tickets = TicketSerializerSessionCreate(many=True, write_only=True)
+    
     class Meta:
         model = Session
         fields = '__all__'
+    
+    def validate_tickets(self, tickets):
+        tickets.sort(key=lambda ticket: ticket['order'])
+        for ind, ticket in enumerate(tickets):
+            if ind > 0 and tickets[ind]['order'] == tickets[ind-1]['order']:
+                raise serializers.ValidationError('Invalid ordering!')
+        
+        tickets.sort(key=lambda ticket: ticket['ticket_id'])
+        for ind, ticket in enumerate(tickets):
+            if ind > 0 and tickets[ind]['ticket_id'] == tickets[ind-1]['ticket_id']:
+                raise serializers.ValidationError('Invalid ticket ID!')
+        return super().validate(tickets)
     
     def validate_pokerboard(self, attrs):
         """
@@ -30,3 +53,17 @@ class SessionSerializer(serializers.ModelSerializer):
             )
         return attrs
 
+    def create(self, validated_data):
+        """
+        Updating ticket orders and creating new session.
+        """
+        tickets = {ticket['ticket_id']: ticket['order'] for ticket in validated_data['tickets']}
+        whens = [When(ticket_id=ticket_id, then=Value(tickets[ticket_id])) for ticket_id in tickets.keys()]
+        validated_data.pop('tickets')
+        Ticket.objects.filter(ticket_id__in=tickets).update(
+            order=Case(
+                *whens,
+                default=F('order')
+            ),
+        )
+        return super().create(validated_data)
--- session/views.py
+++ session/views.py
@@ -1,9 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import serializers
-
-from pokerboard.models import Ticket
 
 from session.models import Session
 from session.serializers import SessionSerializer
@@ -28,20 +25,3 @@ class SessionViewSet(viewsets.ModelViewSet):
             Session, pokerboard_id=pokerboard_id, status=Session.ONGOING
         )
         return active_session
-
-    def create(self, request, *args, **kwargs):
-        tickets = request.data['tickets']
-        tickets.sort(key = lambda x : x['order'])
-        for ind, ticket in enumerate(tickets):
-            if ind > 0 and tickets[ind]['order'] == tickets[ind-1]['order']:
-                raise serializers.ValidationError('Invalid ordering!')
-        
-        ticket_queryset = []
-        for ticket in tickets:
-            ticket_ = get_object_or_404(Ticket, ticket_id=ticket['ticket_id'])
-            ticket_.order = ticket['order']
-            ticket_queryset.append(ticket_)
-        
-        res = super().create(request, *args, **kwargs)
-        Ticket.objects.bulk_update(ticket_queryset, ['order'])
-        return res
